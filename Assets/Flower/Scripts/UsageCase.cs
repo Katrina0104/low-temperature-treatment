158b09049be89b042a1fd7521cccd42b7a335210
Katrina0104 <katrina940104@gmail.com>
2025-11-11 18:23:53 +0800

    flower system update

--- Assets/Flower/Scripts/UsageCase.cs
+++ Assets/Flower/Scripts/UsageCase.cs
@@ -3,15 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Flower;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class UsageCase : MonoBehaviour
 {
     FlowerSystem flowerSys;
     private string myName;
     private int progress = 0;
-    private bool pickedUpTheKey = false;
+    private bool breathingRate = false;
     private bool isGameEnd = false;
     private bool isLocked = false;
+
+    // VR devices
+    [SerializeField] InputActionReference rightTriggerAction;
+    bool triggerPressedLastFrame;
+    bool rightTriggerEnabled = false;
+    private bool canAcceptVRInput = true; // 新增：控制是否接受VR輸入
+    private bool triggerPressed = false;
 
     void Start()
     {
@@ -27,6 +37,7 @@ public class UsageCase : MonoBehaviour
 
         flowerSys = FlowerManager.Instance.CreateFlowerSystem("FlowerSample", false);
         flowerSys.SetupDialog();
+        //flowerSys.SetupButton();
         flowerSys.SetupUIStage();
 
         // 將 Nurse01 注入 FlowerSystem
@@ -43,6 +54,40 @@ public class UsageCase : MonoBehaviour
         // 註冊自訂指令和特效
         flowerSys.RegisterCommand("UsageCase", CustomizedFunction);
         flowerSys.RegisterEffect("customizedRotation", EffectCustomizedRotation);
+
+        // VR
+        SetupVRInput();
+    }
+
+    void OnEnable()
+    {
+        if (rightTriggerAction?.action != null)
+        {
+            rightTriggerAction.action.Enable();
+            rightTriggerEnabled = true;
+            Debug.Log("RightTrigger Action Enabled");
+        }
+        else
+        {
+            Debug.LogWarning("RightTrigger Action Reference is null on Enable!");
+        }
+    }
+
+    void OnDisable()
+    {
+        if (rightTriggerAction?.action != null)
+        {
+            rightTriggerAction.action.Disable();
+            Debug.Log("RightTrigger Action Disabled");
+        }
+    }
+    void OnDestroy()
+    {
+        if (rightTriggerAction?.action != null)
+        {
+            rightTriggerAction.action.started -= OnRightTriggerStarted;
+            rightTriggerAction.action.canceled -= OnRightTriggerCanceled;
+        }
     }
 
     void Update()
@@ -55,31 +100,44 @@ public class UsageCase : MonoBehaviour
             switch (progress)
             {
                 case 0:
-                    flowerSys.ReadTextFromResource("NPC_nurse01");
-                    progress++; // 避免重複
+                    flowerSys.ReadTextFromResource("NPC_nurse01(1)");
                     break;
-                    /* 可延伸更多進度
-                    case 1:
-                        flowerSys.ReadTextFromResource("NPC_nurse01_2");
-                        progress++;
-                        break;
-                    */
+
+                case 1:
+                    // 當顯示按鈕時，暫時禁用VR輸入
+                    canAcceptVRInput = false;
+                    flowerSys.SetupButtonGroup();
+                    flowerSys.SetupButton("Yes", () => {
+                        breathingRate = true;
+                        flowerSys.Resume();
+                        flowerSys.RemoveButtonGroup();
+                        flowerSys.ReadTextFromResource("NPC_nurse01(2)");
+                        progress = 2;
+                        canAcceptVRInput = true; // 重新啟用VR輸入
+                    });
+                    flowerSys.SetupButton("No", () => {
+                        flowerSys.Resume();
+                        flowerSys.RemoveButtonGroup();
+                        flowerSys.ReadTextFromResource("retry");
+                        progress = 1;
+                        canAcceptVRInput = true; // 重新啟用VR輸入
+                    });
+                    break;
+
+                case 2:
+                    break;
             }
+            progress++;
         }
 
         if (!isGameEnd)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                flowerSys.Next(); // 下一句
-            }
             if (Input.GetKeyDown(KeyCode.R))
             {
-                flowerSys.Resume(); // 繼續
+                flowerSys.Resume();
             }
         }
     }
-
     private void CustomizedFunction(List<string> _params)
     {
         var resultValue = int.Parse(_params[0]) + int.Parse(_params[1]);
@@ -107,6 +165,38 @@ public class UsageCase : MonoBehaviour
         catch (Exception)
         {
             Debug.LogError($"Effect - CustomizedRotation @ [{key}] failed.");
+        }
+    }
+    void SetupVRInput()
+    {
+        if (rightTriggerAction?.action != null)
+        {
+            rightTriggerAction.action.started += OnRightTriggerStarted;
+            rightTriggerAction.action.canceled += OnRightTriggerCanceled;
+            rightTriggerAction.action.Enable();
+        }
+    }
+    private void OnRightTriggerStarted(InputAction.CallbackContext context)
+    {
+        if (!canAcceptVRInput || flowerSys == null || isGameEnd || isLocked) return;
+
+        if (context.ReadValue<float>() > 0.1f)
+        {
+            triggerPressed = true;
+            HandleVRTriggerPress();
+        }
+    }
+
+    private void OnRightTriggerCanceled(InputAction.CallbackContext context)
+    {
+        triggerPressed = false;
+    }
+
+    private void HandleVRTriggerPress()
+    {
+        if (flowerSys != null && canAcceptVRInput && !isGameEnd && !isLocked)
+        {
+            flowerSys.Next();
         }
     }
 }
